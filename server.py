0612802635d7a452598611eebc7984f2e40b6439
Lawrence Thatcher <lwthatcher@msn.com>
2018-04-05 17:53:56 -0600

    sever now serves HTML files from templates folder

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
+import flask
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return flask.render_template('index.html', foo=42)
+
+
+@app.route('/data')
+def dataset():
+    return flask.render_template('data_A.html')
