238f8c05375d11640c12477b801da999ef0f5bd0
Lawrence Thatcher <lwthatcher@msn.com>
2018-04-05 18:26:04 -0600

    opens matplotlib when navigating to /data route, but hangs until plot is closed

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
 import flask
 from flask import Flask
-app = Flask(__name__)
+import subprocess
+# from .plot import plot_the_stupid_dataset_in_glorious_3d
 
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return flask.render_template('index.html', foo=42)
 
-
 @app.route('/data')
 def dataset():
+    subprocess.run(["python3", "plot.py"])
     return flask.render_template('data_A.html')
+
+if __name__ == '__main__':
+    app.run()
